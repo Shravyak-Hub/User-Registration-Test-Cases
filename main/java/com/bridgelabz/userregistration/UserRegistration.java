94ebcf6417abf56833c4a3d4371b6774912c01a4
Shravyak-Hub <shravyak156@gmail.com>
2020-10-22 12:12:23 +0530

    Modified file: UserRegistration.java, added code to validate password

--- main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -5,6 +5,7 @@ public class UserRegistration {
     public static final String NAME_REGEX = "^[A-Z][a-z]{2,}$";
     public static final String EMAIL_REGEX = "^([a-z0-9]+[-._+]?[a-z0-9]+)+@[a-z0-9-]+.[a-z]{2,3}.[a-z]{2,3}$";
     public static final String PHONE_NUMBER_REGEX = "^[9][1][ ][6-9][0-9]{9}$";
+    public static final String PASSWORD_REGEX = "^.{8,}$";
 
     public boolean validateFirstName(String firstName) {
         return firstName.matches(NAME_REGEX);
@@ -20,6 +21,10 @@ public class UserRegistration {
 
     public boolean validatePhoneNumber(String phoneNumber) {
         return phoneNumber.matches(PHONE_NUMBER_REGEX);
+    }
+
+    public boolean validatePassword(String password) {
+        return password.matches(PASSWORD_REGEX);
     }
 
 }
