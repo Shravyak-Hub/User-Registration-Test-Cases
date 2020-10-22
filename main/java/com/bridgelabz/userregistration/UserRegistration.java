2a368d2cd089a71e923165c20709128d92caf691
Shravyak-Hub <shravyak156@gmail.com>
2020-10-22 11:46:53 +0530

    Added file: UserRegistration.java, added code to validate first name

---
+++ main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.userregistration;
+
+public class UserRegistration {
+
+    public static final String NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+
+    public boolean validateFirstName(String firstName) {
+        return firstName.matches(NAME_REGEX);
+    }
+
+}
