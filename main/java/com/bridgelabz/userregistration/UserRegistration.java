8a7779ea1498a989608b5ca481fc32c42c9f90d3
Shravyak-Hub <shravyak156@gmail.com>
2020-10-22 12:29:25 +0530

    Modified file: UserRegistration.java, added code to validate password with at least one number

--- main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -5,7 +5,7 @@ public class UserRegistration {
     public static final String NAME_REGEX = "^[A-Z][a-z]{2,}$";
     public static final String EMAIL_REGEX = "^([a-z0-9]+[-._+]?[a-z0-9]+)+@[a-z0-9-]+.[a-z]{2,3}.[a-z]{2,3}$";
     public static final String PHONE_NUMBER_REGEX = "^[9][1][ ][6-9][0-9]{9}$";
-    public static final String PASSWORD_REGEX = "^(?=.*[A-Z]).{8,}$";
+    public static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[A-Z]).{8,}$";
 
     public boolean validateFirstName(String firstName) {
         return firstName.matches(NAME_REGEX);
