93e3816cdb6bdf8943ffd9c6f17a7dcaf3a10c0f
Shravyak-Hub <shravyak156@gmail.com>
2020-10-22 12:01:25 +0530

    Modified file: UserRegistration.java, added code to validate email

--- main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -2,7 +2,8 @@ package com.bridgelabz.userregistration;
 
 public class UserRegistration {
 
-    public static final String NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+    public static final String NAME_REGEX = "^[A-Z][a-z]{2,}$";
+    public static final String EMAIL_REGEX = "^([a-z0-9]+[-._+]?[a-z0-9]+)+@[a-z0-9-]+.[a-z]{2,3}.[a-z]{2,3}$";
 
     public boolean validateFirstName(String firstName) {
         return firstName.matches(NAME_REGEX);
@@ -10,6 +11,10 @@ public class UserRegistration {
 
     public boolean validateLastName(String lastName) {
         return lastName.matches(NAME_REGEX);
+    }
+
+    public boolean validateEmail(String email) {
+        return email.matches(EMAIL_REGEX);
     }
 
 }
