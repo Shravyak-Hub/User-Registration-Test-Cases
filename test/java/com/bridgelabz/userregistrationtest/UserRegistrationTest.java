b07e1eacadbc2056a07603994ddb06e150173860
Shravyak-Hub <shravyak156@gmail.com>
2020-10-22 16:44:34 +0530

    Modified file: UserRegistrationTest.java, added negative scenarios: when last name is less than three letters and when first letter is not uppercase

--- test/java/com/bridgelabz/userregistrationtest/UserRegistrationTest.java
+++ test/java/com/bridgelabz/userregistrationtest/UserRegistrationTest.java
@@ -14,10 +14,38 @@ public class UserRegistrationTest {
     }
 
     @Test
+    public void givenFirstName_WhenLessThanThreeLetters_ShouldReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean validation = userRegistration.validateFirstName("Sh");
+        Assert.assertFalse(validation);
+    }
+
+    @Test
+    public void givenFirstName_WhenFirstLetterNotUppercase_ShouldReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean validation = userRegistration.validateFirstName("shravya");
+        Assert.assertFalse(validation);
+    }
+
+    @Test
     public void givenLastName_WhenValid_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean validation = userRegistration.validateLastName("Kotha");
         Assert.assertTrue(validation);
+    }
+
+    @Test
+    public void givenLastName_WhenLessThanThreeLetters_ShouldReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean validation = userRegistration.validateLastName("Ko");
+        Assert.assertFalse(validation);
+    }
+
+    @Test
+    public void givenLastName_WhenFirstLetterNotUppercase_ShouldReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean validation = userRegistration.validateLastName("kotha");
+        Assert.assertFalse(validation);
     }
 
 }
